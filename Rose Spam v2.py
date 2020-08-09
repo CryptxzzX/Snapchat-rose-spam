a2cf318a79b18699b8202c51c1b9df4dbfb2b7cd
CryptxzzX <40187463+CryptxzzX@users.noreply.github.com>
2020-08-09 14:58:21 -0300

    Add files via upload

---
+++ Rose Spam v2.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+from os import system, name 
+import time
+import random
+root = tk.Tk()
+
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+            
+
+def startSpam():
+    messages = ["your a fuckin loser", "go fuck yourself bud", "quit ur shit you fucking junkie", "mirrors can't talk. Lucky for you, they cant laugh either.", "if I had a face like yours, I'd sue my parents.", "If I said anything to offend you it was purely intentional.", "If I throw a stick, will you go away? fucking dog", "You started at the bottom and it's been downhill ever since"]
+    url = 'https://onrose.co/cryptxzz'
+    driver = webdriver.Chrome("C:/Users/Carter/Desktop/Python Development/chromedriver")
+    clear()
+    for y in range(0, 10):  
+        driver.get(url)    
+        time.sleep(2)
+        driver.find_element_by_id('message-textarea').send_keys(random.choice(messages))
+        time.sleep(1)
+        driver.find_element_by_id('message-submit').click()
+        print("Sent successfully on time %d" % (y))
+    driver.close()
+
+start = tk.Button(root, text="Start Spam", padx=10, pady=5, fg="black", command=startSpam)
+start.pack()
+root.mainloop()
